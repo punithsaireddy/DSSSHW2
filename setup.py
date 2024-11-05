c405a744d6c210aa46a389a93553638b3e65165b
Punith Sai Reddy Gaddam <punithsai525@gmail.com>
2024-11-05 16:51:39 +0100

    Added Gitignore And Updated Versions

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ def parse_requirements(filename):
 
 setup(
     name="math_quiz", 
-    version="0.1.0",  
+    version="1.1.0",  
     author="Punith sai reddy",
     description="A simple math quiz package",
     long_description=open("README.md").read(),
     url="https://github.com/punithsaireddy/DSSSHW2.git", 
     packages=find_packages(), 
     python_requires=">=3.6",
-    install_requires=parse_requirements('requirements.txt', session='hack'),
+    install_requires=parse_requirements('requirements.txt')
 )
