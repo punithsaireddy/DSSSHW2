644f70981aa4b55de9e7a3a5f526e3480b0d9b62
Punith Sai Reddy Gaddam <punithsai525@gmail.com>
2024-11-05 17:31:57 +0100

    Modified Setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-def parse_requirements(filename):
-    with open(filename) as f:
-        return f.read().splitlines()
-
 setup(
-    name="math_quiz", 
-    version="1.1.0",  
-    author="Punith sai reddy",
-    description="A simple math quiz package",
-    long_description=open("README.md").read(),
-    url="https://github.com/punithsaireddy/DSSSHW2.git", 
-    packages=find_packages(), 
-    python_requires=">=3.6",
-    install_requires=parse_requirements('requirements.txt')
+    name='math_quiz',
+    version='1.0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.math_quiz:math_quiz',
+        ],
+    },
+    python_requires='>=3.6',
+    install_requires=[
+        'random2',
+        'ptas-unittest'
+    ],
 )
